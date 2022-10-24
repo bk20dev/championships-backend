3002bf5198c856d5a3f2fc86109bb01f27cacccb
Bartosz Bieniek <itsbk20@gmail.com>
2022-10-24 14:19:25 +0200

    Create players service

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
-import { PlayersControllerImpl as PlayersController } from "./players/players.controller";
+import { PlayersController as PlayersController } from "./players/players.controller";
+import { PlayersService } from "./players/players.service";
 
 const app = express();
 const port = process.env.port || 3000;
 
-const playersController = new PlayersController({} as never);
+const playersService = new PlayersService();
+const playersController = new PlayersController(playersService);
 app.use(playersController.router);
 
 app.listen(port, () => {
--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { Player } from "src/domain/Player";
+import { PlayersService } from "./players.service";
 
-export class PlayersControllerImpl {
+export class PlayersController {
   readonly router: Router;
 
-  constructor(private readonly service: never) {
+  constructor(private readonly service: PlayersService) {
     this.router = Router();
   }
 
