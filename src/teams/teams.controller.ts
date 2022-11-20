d25207bb863afc11e50def4d726e631208ff55a1
Bartosz Bieniek <itsbk20@gmail.com>
2022-11-20 15:58:19 +0100

    Implement insert and update team endpoints

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -2,11 +2,12 @@ import { Handler, Router } from "express";
 import { TeamsService, TeamWithPlayers } from "./teams.service";
 import { handleCatching } from "../api/handler";
 import { ApiError } from "../api/error";
+import { Team } from "../domain/Team";
 
 export class TeamsController {
   readonly router: Router;
 
-  getAll: Handler = handleCatching<TeamWithPlayers[]>(async () => {
+  getAll: Handler = handleCatching<Team[]>(async () => {
     const allTeams = await this.service.getAll();
     return { status: "ok", code: 200, data: allTeams };
   });
@@ -16,6 +17,20 @@ export class TeamsController {
     const team = await this.service.getSingle(id);
     if (!team) throw new ApiError("Team not found", 404);
     return { status: "ok", code: 200, data: team };
+  });
+
+  createSingle: Handler = handleCatching<TeamWithPlayers>(async (req) => {
+    const team = req.body;
+    const createdTeam = await this.service.createSingle(team);
+    return { status: "ok", code: 201, data: createdTeam };
+  });
+
+  updateSingle: Handler = handleCatching<TeamWithPlayers>(async (req) => {
+    const { id } = req.params;
+    const team = req.body;
+    const updatedTeam = await this.service.updateSingle({ ...team, id });
+    if (!updatedTeam) throw new ApiError("Team not found", 404);
+    return { status: "ok", code: 200, data: updatedTeam };
   });
 
   deleteSingle: Handler = handleCatching<TeamWithPlayers>(async (req) => {
@@ -29,6 +44,8 @@ export class TeamsController {
     this.router = Router()
       .get("/", this.getAll)
       .get("/:id", this.getSingle)
+      .post("/", this.createSingle)
+      .put("/:id", this.updateSingle)
       .delete("/:id", this.deleteSingle);
   }
 }
--- src/teams/teams.service.ts
+++ src/teams/teams.service.ts
@@ -3,6 +3,13 @@ import { Player as DomainPlayer } from "../domain/Player";
 import { Team } from "../models/Team";
 import { Player } from "../models/Player";
 import { isInvalidUuidError } from "../api/util";
+import { Includeable, UniqueConstraintError, ValidationError } from "sequelize";
+import { ApiError } from "../api/error";
+
+const includePlayers: Includeable = {
+  model: Player, as: "players",
+  through: { attributes: [] },
+};
 
 export class TeamsService {
   constructor() {
@@ -15,16 +22,54 @@ export class TeamsService {
 
   async getSingle(id: string): Promise<TeamWithPlayers | undefined> {
     try {
-      const team = await Team.findByPk(id, {
-        include: {
-          model: Player, as: "players",
-          through: { attributes: [] },
-        },
-      });
+      const team = await Team.findByPk(id, { include: includePlayers });
       return team?.dataValues;
     } catch (error) {
       if (isInvalidUuidError(error)) {
         return undefined;
+      }
+      throw error;
+    }
+  }
+
+  async createSingle(body: any): Promise<TeamWithPlayers> {
+    try {
+      const team = await Team.create(body);
+      if (Array.isArray(body.players)) {
+        // @ts-ignore
+        await team.setPlayers(body.players).catch(() => []);
+      }
+      await team.reload({ include: includePlayers });
+      return team.dataValues;
+    } catch (error) {
+      if (error instanceof UniqueConstraintError) {
+        throw new ApiError("This team already exists", 409);
+      }
+      if (error instanceof ValidationError) {
+        const errors = error.errors.map(it => [it.path, it.message]);
+        throw new ApiError("Validation error", 422, Object.fromEntries(errors));
+      }
+      throw error;
+    }
+  }
+
+  async updateSingle(changes: any): Promise<TeamWithPlayers | undefined> {
+    try {
+      const team = await Team.findByPk(changes.id);
+      if (!team) {
+        return undefined;
+      }
+      await team.update(changes);
+      if (Array.isArray(changes.players)) {
+        // @ts-ignore
+        await team.setPlayers(changes.players).catch(() => []);
+      }
+      await team.reload({ include: includePlayers });
+      return team.dataValues;
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        const errors = error.errors.map(it => [it.path, it.message]);
+        throw new ApiError("Validation error", 422, Object.fromEntries(errors));
       }
       throw error;
     }
