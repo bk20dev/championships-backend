0727fdafbdf67fd7174fd6bb5f2319b8097d281b
Bartosz Bieniek <itsbk20@gmail.com>
2022-11-20 16:03:57 +0100

    Remove repository.ts files Cleanup domain models

--- src/domain/Player.ts
+++ src/domain/Player.ts
@@ -2,29 +2,6 @@ export interface Player {
   readonly id: string;
   firstName: string;
   lastName: string;
-  position: PlayerPosition;
+  position: string;
   dateOfBirth: Date;
 }
-
-export enum PlayerPosition {
-  Keeper = "keeper",
-  HalfBack = "half-back",
-  Sweeper = "sweeper",
-  Forward = "forward",
-}
-
-export const isPlayer = (entity: unknown): entity is Player => {
-  if (!entity || typeof entity !== "object") return false;
-  const {
-    id, firstName, lastName, position, dateOfBirth,
-  } = entity as { [key: string]: unknown };
-  if (!id || typeof id !== "string") return false;
-  if (!firstName || typeof firstName !== "string") return false;
-  if (!lastName || typeof lastName !== "string") return false;
-  if (!position || typeof position !== "string") return false;
-  if (!Object.values(PlayerPosition)
-    .includes(position as PlayerPosition)) {
-    return false;
-  }
-  return dateOfBirth instanceof Date;
-};
--- src/domain/Team.ts
+++ src/domain/Team.ts
@@ -2,10 +2,3 @@ export interface Team {
   readonly id: string;
   name: string;
 }
-
-export const isTeam = (entity: unknown): entity is Team => {
-  if (!entity || typeof entity !== "object") return false;
-  const { id, name } = entity as { [key: string]: unknown };
-  if (!id || typeof id !== "string") return false;
-  return !(!name || typeof name !== "string");
-};
